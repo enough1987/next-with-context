565b7756d3c4fa0e1e1414ab640ee663b14a953c
oleksandr.tilha <oleksandr.tilha@nordstrom.com>
2023-10-03 16:35:19 +0300

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,24 @@ import styles from '@/styles/Home.module.css'
 import Header from '@/components/header'
 import ButtonLink from '@/components/buttonLink'
 import React from 'react'
+import { GetServerSideProps } from 'next/types'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default React.memo(function Home() {
+
+export const getServerSideProps = (async (_context) => {
+  const data = [1,2,3,4,5,6,7,8,9,10];
+
+  return { props: { data } }
+}) satisfies GetServerSideProps<{
+  data: number[]
+}>
+
+interface IHomeProps {
+  data: number[]
+}
+
+export default React.memo(function Home({ data }: IHomeProps) {
   return (
     <>
       <Head>
@@ -21,6 +35,9 @@ export default React.memo(function Home() {
         <ButtonLink name="About" href="/about" />
        </Header>
       <main className={`${styles.main} ${inter.className}`}>
+        <ul>
+          {data.map((item) => <li key={item}>{item}</li>)}
+        </ul>
       </main>
     </>
   )
